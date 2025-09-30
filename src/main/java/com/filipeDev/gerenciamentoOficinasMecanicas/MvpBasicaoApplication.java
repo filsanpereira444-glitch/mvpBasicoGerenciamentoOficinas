22c7d120a0c599284ced236a723eb394fc3615ec
filsanpereira444-glitch <filsanpereira444@gmail.com>
2025-09-29 23:00:51 -0300

    Criação do Projeto

---
+++ src/main/java/com/filipeDev/gerenciamentoOficinasMecanicas/MvpBasicaoApplication.java
@@ -0,0 +1,13 @@
+package com.filipeDev.gerenciamentoOficinasMecanicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpBasicaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpBasicaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipeDev/gerenciamentoOficinasMecanicas/MvpBasicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipeDev.gerenciamentoOficinasMecanicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpBasicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
